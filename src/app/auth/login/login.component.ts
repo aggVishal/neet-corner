b95b7ff1b577b844badf9c6dc81788ada057d4b3
Vishal Aggarwal <aggarwalvishal901@gmail.com>
2020-11-24 18:39:17 +0530

    online test added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,20 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  public result = "";
-  username: any;
-  password: any;
   constructor() { }
-
-  loginUser() {
-    if (this.username != "admin" || this.password != "admin") {
-      this.result = "Wrong username or password";
-      // console.log("done");
-    }
-    else {
-      this.result = "";
-    }
-  }
 
   ngOnInit(): void {
   }
